f391383f6b96265b07baf46e115912137d660f6b
victra <victra.sonata@ottodigital.id>
2022-06-10 06:23:28 +0700

    uraaa

---
+++ golang/goroutin.go
@@ -0,0 +1,28 @@
+package golang
+
+import "fmt"
+import "runtime"
+
+func golang() {
+	runtime.GOMAXPROCS(2)
+
+	var messages = make(chan string)
+
+	var sayHelloTo = func(who string) {
+		var data = fmt.Sprintf("hello %s", who)
+		messages <- data
+	}
+
+	go sayHelloTo("john wick")
+	go sayHelloTo("ethan hunt")
+	go sayHelloTo("jason bourne")
+
+	var message1 = <-messages
+	fmt.Println(message1)
+
+	var message2 = <-messages
+	fmt.Println(message2)
+
+	var message3 = <-messages
+	fmt.Println(message3)
+}
